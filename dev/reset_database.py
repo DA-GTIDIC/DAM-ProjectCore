431c904eb78e2512a693bf0169c038700268cdb0
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2021-02-23 13:31:22 +0100

    Updating user model. Created nure enum to model different roles and updated the database script to create randomly 20 users.

--- dev/reset_database.py
+++ dev/reset_database.py
@@ -12,10 +12,28 @@ import settings
 from db.models import SQLAlchemyBase, User, GenereEnum, RolEnum, UserToken, Event, EventTypeEnum
 from settings import DEFAULT_LANGUAGE
 
+import random
+import string
+
 # LOGGING
 mylogger = logging.getLogger(__name__)
 settings.configure_logging()
 
+def randomGenre():
+    if bool(random.getrandbits(1)):
+        return GenereEnum.female
+    else: 
+        return GenereEnum.male
+
+def randomRol():
+    if bool(random.getrandbits(1)):
+        return RolEnum.freemium
+    else: 
+        return RolEnum.premium
+
+def randomString(length):
+    password = ''.join([random.choice( string.ascii_letters + string.digits) for n in range(length)])
+    return password
 
 def execute_sql_file(sql_file):
     sql_folder_path = os.path.join(os.path.dirname(__file__), "sql")
@@ -53,292 +71,28 @@ if __name__ == "__main__":
     )
     user_admin.set_password("DAMCoure")
 
-    # noinspection PyArgumentList
-    user_1= User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="usuari1",
-        email="usuari1@gmail.com",
-        name="usuari",
-        surname="1",
-        birthdate=datetime.datetime(1989, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.premium,
-    )
-    user_1.set_password("a1s2d3f4")
-    user_1.tokens.append(UserToken(token="656e50e154865a5dc469b80437ed2f963b8f58c8857b66c9bf"))
-
-    # noinspection PyArgumentList
-    user_2 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user2",
-        email="user2@gmail.com",
-        name="user",
-        surname="2",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.freemium,
-    )
-    user_2.set_password("r45tgt")
-    user_2.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f584b2"))
-
-    # noinspection PyArgumentList
-    user_3 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user3",
-        email="user3@gmail.com",
-        name="user",
-        surname="3",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.female,
-        rol=RolEnum.freemium,
-    )
-    user_3.set_password("r45tgt")
-    user_3.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f584b3"))
-
-    # noinspection PyArgumentList
-    user_4 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user4",
-        email="user4@gmail.com",
-        name="user",
-        surname="4",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.female,
-        rol=RolEnum.freemium,
-    )
-    user_4.set_password("r45tgt")
-    user_4.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f584b4"))
-
-    # noinspection PyArgumentList
-    user_5 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user5",
-        email="user5@gmail.com",
-        name="user",
-        surname="5",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.premium,
-    )
-    user_5.set_password("r45tgt")
-    user_5.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f584b5"))
-
-    # noinspection PyArgumentList
-    user_6 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user6",
-        email="user6@gmail.com",
-        name="user",
-        surname="6",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.female,
-        rol=RolEnum.freemium,
-    )
-    user_6.set_password("r45tgt")
-    user_6.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f584b6"))
-
-    # noinspection PyArgumentList
-    user_7 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user7",
-        email="user7@gmail.com",
-        name="user",
-        surname="7",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.freemium,
-    )
-    user_7.set_password("r45tgt")
-    user_7.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f584b7"))
-
-    # noinspection PyArgumentList
-    user_8 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user8",
-        email="user8@gmail.com",
-        name="user",
-        surname="8",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.female,
-        rol=RolEnum.premium,
-    )
-    user_8.set_password("r45tgt")
-    user_8.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f584b8"))
-
-    # noinspection PyArgumentList
-    user_9 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user9",
-        email="user9@gmail.com",
-        name="user",
-        surname="9",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.premium,
-    )
-    user_9.set_password("r45tgt")
-    user_9.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f584b9"))
-
-    # noinspection PyArgumentList
-    user_10 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user10",
-        email="user10@gmail.com",
-        name="user",
-        surname="10",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.premium,
-    )
-    user_10.set_password("r45tgt")
-    user_10.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f585b2"))
-
-    # noinspection PyArgumentList
-    user_11 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user11",
-        email="user11@gmail.com",
-        name="user",
-        surname="11",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.female,
-        rol=RolEnum.freemium,
-    )
-    user_11.set_password("r45tgt")
-    user_11.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f586b2"))
-
-    # noinspection PyArgumentList
-    user_12 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user12",
-        email="user12@gmail.com",
-        name="user",
-        surname="12",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.freemium,
-    )
-    user_12.set_password("r45tgt")
-    user_12.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f587b2"))
-
-    # noinspection PyArgumentList
-    user_13 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user13",
-        email="user13@gmail.com",
-        name="user",
-        surname="13",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.freemium,
-    )
-    user_13.set_password("r45tgt")
-    user_13.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f588b2"))
-
-    # noinspection PyArgumentList
-    user_14 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user14",
-        email="user14@gmail.com",
-        name="user",
-        surname="14",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.premium,
-    )
-    user_14.set_password("r45tgt")
-    user_14.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f589b2"))
-
-    # noinspection PyArgumentList
-    user_15 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user15",
-        email="user15@gmail.com",
-        name="user",
-        surname="15",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.female,
-        rol=RolEnum.freemium,
-    )
-    user_15.set_password("r45tgt")
-    user_15.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f684b2"))
-
-    # noinspection PyArgumentList
-    user_16 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user16",
-        email="user16@gmail.com",
-        name="user",
-        surname="16",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.female,
-        rol=RolEnum.premium,
-    )
-    user_16.set_password("r45tgt")
-    user_16.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f784b2"))
-
-    # noinspection PyArgumentList
-    user_17 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user17",
-        email="user17@gmail.com",
-        name="user",
-        surname="17",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.female,
-        rol=RolEnum.premium,
-    )
-    user_17.set_password("r45tgt")
-    user_17.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f884b2"))
-
-    # noinspection PyArgumentList
-    user_18 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user18",
-        email="user18@gmail.com",
-        name="user",
-        surname="18",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.male,
-        rol=RolEnum.freemium,
-    )
-    user_18.set_password("r45tgt")
-    user_18.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f429f984b2"))
-
-    # noinspection PyArgumentList
-    user_19 = User(
-        created_at=datetime.datetime(2020, 1, 1, 0, 1, 1),
-        username="user19",
-        email="user19@gmail.com",
-        name="user",
-        surname="19",
-        birthdate=datetime.datetime(2017, 1, 1),
-        genere=GenereEnum.female,
-        rol=RolEnum.premium,
-    )
-    user_19.set_password("r45tgt")
-    user_19.tokens.append(UserToken(token="0a821f8ce58965eadc5ef884cf6f7ad99e0e7f58f439f584b2"))
-
     db_session.add(user_admin)
-    db_session.add(user_1)
-    db_session.add(user_2)
-    db_session.add(user_3)
-    db_session.add(user_4)
-    db_session.add(user_5)
-    db_session.add(user_6)
-    db_session.add(user_7)
-    db_session.add(user_8)
-    db_session.add(user_9)
-    db_session.add(user_10)
-    db_session.add(user_11)
-    db_session.add(user_12)
-    db_session.add(user_13)
-    db_session.add(user_14)
-    db_session.add(user_15)
-    db_session.add(user_16)
-    db_session.add(user_17)
-    db_session.add(user_18)
-    db_session.add(user_19)
+
+    users = []
+
+    for i in range(1, 20):
+        aux_user = User(
+            created_at=datetime.datetime(random.randint(2000, 2021), random.randint(1, 12), random.randint(1, 31)),
+            username="user"+str(i),
+            email="user"+str(i)+"@gmail.com",
+            name="user",
+            surname=str(i),
+            birthdate=datetime.datetime(random.randint(1920, 2021), random.randint(1, 12), random.randint(1, 31)),
+            genere=randomGenre(),
+            rol=randomRol(),
+        )
+        aux_user.set_password(randomString(random.randint(4,8)))
+        aux_user.tokens.append(UserToken(token=randomString(50)))
+
+        users.append(aux_user)
+        
+    for user in users:
+        db_session.add(user)
 
 
     # -------------------- CREATE EVENTS --------------------
@@ -354,7 +108,7 @@ if __name__ == "__main__":
         finish_date=datetime.datetime.now() + (day_period * 5),
         owner_id = 0,
         poster="logo.png",
-        registered=[user_1, user_2]
+        registered=users
     )
 
     event_livecoding = Event(
@@ -365,7 +119,7 @@ if __name__ == "__main__":
         start_date=datetime.datetime.now() - (day_period * 5),
         finish_date=datetime.datetime.now() - (day_period * 4),
         owner_id=1,
-        registered=[user_2]
+        registered=[users[4]]
     )
 
     event_lanparty = Event(
